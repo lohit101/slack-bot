799348b31bef4662a3dfab071f13b59b72b65197
lohit101 <lohitchhabra101@gmail.com>
2025-04-02 23:37:13 +0530

    Bot communicating with the server properly [23:36 02.04.2025]

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 
+const { WebClient } = require('@slack/web-api');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN); // initialize slack api
 
 // middleware
 app.use(express.json());
@@ -10,6 +14,16 @@ app.use(express.json());
 // test route
 app.get('/', (req, res) => {
     res.send('Bot is running!');
+});
+
+// command endpoint for testing
+app.post('/slack/command', async (req, res) => {
+    console.log('Command received:', req.body); // console logging command for debugging (if required)
+
+    res.json({
+        response_type: 'ephemeral', // ephemeral - i think only visible to the user who called the command
+        text: 'Approval request received!',
+    });
 });
 
 // start server
